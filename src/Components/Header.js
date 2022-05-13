6f7ee18b65165e2b9d890b84376aee05de75cbb4
Aesha <rp804594@gmail.com>
2022-05-13 13:17:21 +0530

    aesha

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <AppBar color="primary" position="static">
+      <Container>
+        <Toolbar>
+          <Typography>Crypto Hunter</Typography>
+          <Select
+            variant="outlined"
+            style={{
+              width: 100,
+              height: 40,
+              marginleft: 15,
+            }}
+          >
+            <MenuItem value={"USD"}> USD </MenuItem>
+            <MenuItem value={"INR"}> INR </MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
-export default Header
+export default Header;
